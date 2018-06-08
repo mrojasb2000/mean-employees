d3f0c2cb202870274d853afc282516bb3faacdf9
Mauricio Rojas <mrojasb2000@gmail.com>
2018-06-08 10:37:20 -0400

    2018-06-08 Add default routes with JSON response.

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ app.use(express.json());
 
 
 // routes
+
+// default route http://localhost:3000
+app.use(require('./routes/default.routes'));
+
+// example: http://localhost:3000/api/employees
 app.use('/api/employees', require('./routes/employee.routes'));
 
 // staring server goes here
