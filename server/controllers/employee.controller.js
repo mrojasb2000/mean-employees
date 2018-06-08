d58d4f3e39e7fcf492d493100c85f4795fbe7505
Mauricio Rojas <mrojasb2000@gmail.com>
2018-06-08 12:25:34 -0400

    2018-06-08 MROJAS Implement methods CRUD on MongoDB.

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,16 +1,42 @@
+const employeeModel = require('../models/employee');
 const employeeCtrl = {};
 
-employeeCtrl.getEmployees = (req, res) => {    
-     res.json({status: 'Api Works.'});
+
+employeeCtrl.getEmployees = (req, res) => {        
+    employeeModel.find()
+        .then(emp =>  res.json(emp))
+        .catch(err => console.log(err));
 };
 
-employeeCtrl.getEmployee = (req, res) => {    
-    res.json({status: 'Api Works.'});
+/*
+employeeCtrl.getEmployees = async (req, res) => {    
+    const employees = await employeeModel.find();        
+    res.json(employees);
+};*/
+   
+
+employeeCtrl.getEmployee =  (req, res) => {       
+    employeeModel.findById(req.params.id)
+        .then(emp =>  res.json(emp))
+        .catch(err => console.log(err));    
 };
 
-employeeCtrl.createEmployee = () => {
 
+employeeCtrl.createEmployee = (req, res) => {
+    const emp = new employeeModel(req.body);
+    console.log(emp);
+    emp.save()
+        .then(db => res.json({'status': 'Employee Saved.'}))
+        .catch(err => console.log(err));
 };
+
+/*employeeCtrl.createEmployee = async (req, res) => {
+    const emp = new employeeModel(req.body);
+    await emp.save();
+    console.log(emp);
+    res.json({'status': 'Employee Saved.'});
+
+};*/
 
 employeeCtrl.updateEmployee = () => {
 
