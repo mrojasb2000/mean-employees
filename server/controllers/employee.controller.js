f5f9c07b5e64c11dc01c9a0239863354899ab5ac
Mauricio Rojas <mrojasb2000@gmail.com>
2018-06-08 11:07:44 -0400

    2018-06-08 MROJAS Add new routes Employee CRUD.

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -4,8 +4,21 @@ employeeCtrl.getEmployees = (req, res) => {
      res.json({status: 'Api Works.'});
 };
 
+employeeCtrl.getEmployee = (req, res) => {    
+    res.json({status: 'Api Works.'});
+};
+
 employeeCtrl.createEmployee = () => {
 
 };
+
+employeeCtrl.updateEmployee = () => {
+
+};
+
+employeeCtrl.deleteEmployee = () => {
+
+};
+
 
 module.exports = employeeCtrl;
--- server/routes/employee.routes.js
+++ server/routes/employee.routes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const employeeDB = require('../controllers/employee.controller');
 
 router.get('/', employeeDB.getEmployees);
+router.post('/', employeeDB.createEmployee);
+router.get('/:id', employeeDB.getEmployee);
+router.put('/:id', employeeDB.updateEmployee);
+router.delete('/:id', employeeDB.deleteEmployee);
 
 module.exports = router;
